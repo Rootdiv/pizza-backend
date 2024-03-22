9376487292b718a8cd8bf70effde81c9c677a3c1
Rootdiv <rootdiv9152@gmail.com>
2024-03-22 14:12:54 +0300

    feat: changed configuration api server

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 // импорт стандартных библиотек Node.js
 const { readFileSync } = require('fs');
-const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
-const { createServer } = require(protocol);
+const { createServer } = require('http');
 const path = require('path');
-
-const options = {};
-if (protocol === 'https') {
-  const domain = 'rootdiv.ru';
-  const certDir = '/etc/nginx/acme.sh';
-  options['key'] = readFileSync(`${certDir}/${domain}/privkey.pem`);
-  options['cert'] = readFileSync(`${certDir}/${domain}/fullchain.pem`);
-}
 
 // файл для базы данных
 const DB_FILE = process.env.DB_FILE || path.resolve(__dirname, 'db.json');
@@ -91,7 +82,7 @@ const getPizzas = params => {
 };
 
 // создаём HTTP сервер, переданная функция будет реагировать на все запросы к нему
-createServer(options, async (req, res) => {
+createServer(async (req, res) => {
   // req - объект с информацией о запросе, res - объект для управления отправляемым ответом
 
   // этот заголовок ответа указывает, что тело ответа будет в JSON формате
@@ -159,10 +150,10 @@ createServer(options, async (req, res) => {
 })
   // выводим инструкцию, как только сервер запустился...
   .on('listening', () => {
-    if (protocol === 'http') {
+    if (process.env.PROD !== 'true') {
       console.log(`Сервер запущен. Вы можете использовать его по адресу http://localhost:${PORT}`);
       console.log('Нажмите CTRL+C, чтобы остановить сервер');
     }
   })
   // ...и вызываем запуск сервера на указанном порту
-  .listen(PORT);
+  .listen(PORT, 'localhost');
